24d278622a70bccbfb93a8870ba701be6503a1ae
John Dimm <john.dimm@gardyn.tech>
2024-10-01 22:01:38 -0700

    Make sure front.jpg comes first.

---
+++ run.sh
@@ -0,0 +1,11 @@
+
+URL_PREFIX=https://raw.githubusercontent.com/johndimm/comic-photos/refs/heads/main/images/
+
+echo "comic_id,area,photo" > comics-images.csv
+
+find ~/projects/comic-photos/images | sed "s#/Users/johndimm/projects/comic-photos/images/##" | grep -E 'jpg|jpeg' | sed -E "s#([^/]*)/#\1,,${URL_PREFIX}\1/#" > tmp-comics-images.csv
+
+cat tmp-comics-images.csv | grep "front.jpg" >> comics-images.csv
+cat tmp-comics-images.csv | grep -v "front.jpg" >> comics-images.csv
+
+cat comics-images.csv
