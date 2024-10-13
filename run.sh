ac8f6293ea8b9ce35450b466b32a8c8f4c9ef142
John Dimm <john.dimm@gardyn.tech>
2024-10-13 11:03:30 -0700

    ff35,36,37

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-
 URL_PREFIX=https://raw.githubusercontent.com/johndimm/comic-photos/refs/heads/main/images/
 
 echo "comic_id,area,photo" > comics-images.csv
@@ -9,3 +8,8 @@ cat tmp-comics-images.csv | grep "front.jpeg" >> comics-images.csv
 cat tmp-comics-images.csv | grep -v "front.jpeg" >> comics-images.csv
 
 cat comics-images.csv
+
+cp ../marvel/data/v2/comics.csv .
+git add images
+git commit .
+git push
